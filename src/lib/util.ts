55fbcd38bca867dc1e012cd61c2c6939f0e829b6
David Cruwys <david@ideasmen.com.au>
2023-01-10 12:09:13 +0700

    update config and util.ts

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -23,11 +23,11 @@ const QUICK_NAVIGATION = {
         {
           title: 'Camtasia (Tips, Tricks, Tutorials)',
           href: '/camtasia'
-        },
-        {
-          title: 'New Home',
-          href: '/home'
         }
+        // {
+        //   title: 'New Home',
+        //   href: '/home'
+        // }
       ]
     }
   ]
@@ -60,12 +60,12 @@ export async function topicNavigation(props: Record<string, number | string | an
 export function slugify(text: string): string {
   return text
     .toString()                           // Cast to string (optional)
-    .normalize('NFKD')            // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
-    .toLowerCase()                  // Convert the string to lowercase letters
-    .trim()                                  // Remove whitespace from both sides of a string (optional)
-    .replace(/\s+/g, '-')            // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')     // Remove all non-word chars
-    .replace(/\-\-+/g, '-');        // Replace multiple - with single -
+    .normalize('NFKD')                    // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+    .toLowerCase()                        // Convert the string to lowercase letters
+    .trim()                               // Remove whitespace from both sides of a string (optional)
+    .replace(/\s+/g, '-')                 // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')             // Remove all non-word chars
+    .replace(/\-\-+/g, '-');              // Replace multiple - with single -
 }
 
 export function topicNavigationPath(file: string): string {
